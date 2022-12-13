9a6fa14c7bd747300fef14a85eed0cea477ceb11
Your Name <your@email.com>
2022-12-13 16:26:54 +0100

    Fixed ypos and added some notes

--- mdat2root.C
+++ mdat2root.C
@@ -1,3 +1,8 @@
+// Convert qmesydaq mdat files to ROOT format
+// For use with MPSD + MPCD setups
+// This version assumes the tubes are stacked vertically such that the x position is along the tube and the y position is the tube ID (contructed from the slotID and modID parameters)
+// Only 3 bits are used for slotID - officially 5 bits are reserved but the 2 most significant bits aren't used (since only 8 double ended tubes can be connected per MPSD)
+
 // -------------------------------------------------------------------//
 // ------------------------- Global variables ------------------------//
 // -------------------------------------------------------------------//
@@ -5,8 +10,7 @@
 // Masks for extracting amp, pos etc. from 48 bit event block
 ULong64_t mask_eventID = 0b100000000000000000000000000000000000000000000000;
 ULong64_t mask_modID =   0b011100000000000000000000000000000000000000000000;
-ULong64_t mask_slotID =  0b000011111000000000000000000000000000000000000000;
-ULong64_t mask_ypos =    0b011111111000000000000000000000000000000000000000;
+ULong64_t mask_slotID =  0b000000111000000000000000000000000000000000000000;
 ULong64_t mask_amp =     0b000000000111111111100000000000000000000000000000;
 ULong64_t mask_xpos =    0b000000000000000000011111111110000000000000000000;
 ULong64_t mask_time =    0b000000000000000000000000000001111111111111111111;
@@ -106,7 +110,7 @@ void ReadEvent(ifstream &infile){
   ReadEntry(infile, rawevent);
   event.eventID = (rawevent & mask_eventID) >> 47;
   event.amp = (rawevent & mask_amp) >> 29;
-  event.ypos = (rawevent & mask_ypos) >> 39;
+  event.ypos = ((rawevent & mask_slotID) >> 39) | ((rawevent & mask_modID) >> 41);
   event.xpos = (rawevent & mask_xpos) >> 19;
   event.modID = (rawevent & mask_modID) >> 44;
   event.slotID = (rawevent & mask_slotID) >> 39;
